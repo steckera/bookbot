5df39769746375120d3aab735df3acf03bd4c304
steckera5 <steckera5@gmail.com>
2025-08-12 18:45:17 -0500

    2: prints a word count to the console from main.py

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
+
+def num_words(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        splitter = file_contents.split()
+        num_words = len(splitter)
+        return num_words
      
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    word_count = num_words("./books/frankenstein.txt")
+    print(f"{word_count} words found in the document")
     
 
 main()
