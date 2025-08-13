207af8dc42787bbb4429632b8c9e8653efdd2348
steckera5 <steckera5@gmail.com>
2025-08-12 22:43:48 -0500

    4: create new functions in stats that count the number of characters that appear in the text and print that info to console in descending order.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
 
 
 def get_book_text(filepath):
@@ -9,8 +9,30 @@ def get_book_text(filepath):
      
 def main():
     file_path = "./books/frankenstein.txt"
-    word_count = get_num_words(get_book_text(file_path))
-    print(f"{word_count} words found in the document")
+    #get text from file
+    text = get_book_text(file_path)
+    #get word count from text file using func in stats
+    word_count = get_num_words(text)
+    #gets individual chars from dict using func in stats
+    char_dict = get_chars_dict(text)
+    #gets sorted list of dicts using func in stats
+    char_list = chars_dict_to_sorted_list(char_dict)
     
+    #print document as requested in lesson    
+    print(f"{word_count} words found in the document")
+    #print(char_count)
+    print(f"""============ BOOKBOT ============
+Analyzing book found at {file_path}...
+----------- Word Count ----------
+Found {word_count} total words
+--------- Character Count -------""")
+    
+    #check each key in char_list to ensure it is a letter and not symbol or number.
+    #then prints each pair.
+    for char_data in char_list:
+        if char_data["char"].isalpha():
+            print(f" {char_data['char']}: {char_data['num']}")
+
+print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,33 @@
+#this gets the text from main, splits it into a list,
+#and returns the length.
 def get_num_words(text):
-        splitter = text.split()
-        num_words = len(splitter)
-        return num_words
+    splitter = text.split()
+    return len(splitter)
+
+#this gets the text from main, sets up an empty dict, and runs a loop through every char. 
+#It lowers each char. Then checks if the char is already in the dict.
+#If the char is alread in the dict, it increases the count. If it isn't in the dict it 
+#adds it. Then it returns an unsorted dict.        
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+ 
+#sets our key to sort with 
+def sort_on(d):
+    return d["num"]
+ 
+#this sets and empty list and then loops through the dict made in get_chars_dict 
+#(pulled from main.) It appends each key value pair to the list as an individual dictionary.
+#Then it sorts the list and returns it. 
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
